042d3e322b9cd6d01a2eb1e15a4b757a4c68705f
Alexander Vasilev <aleksandr.vasilev@arloid.com>
2023-11-27 19:54:07 +0530

    Create EssentialFeed framework

---
+++ EssentialFeed/EssentialFeedTests/EssentialFeedTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import EssentialFeed
+
+final class EssentialFeedTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+}
